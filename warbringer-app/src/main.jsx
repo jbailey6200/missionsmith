a5a5d198abe54c291ed1364c522aef214836f207
jbailey6200 <jeremybailey2312@gmail.com>
2026-01-18 23:53:24 -0600

    analytics

--- warbringer-app/src/main.jsx
+++ warbringer-app/src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Warbringer from './Warbringer'
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Warbringer />
+    <Analytics />
   </React.StrictMode>,
 )
